460cfb489d3612da0ef71d4bd8727d9278f711fa
Karunya Ronith <karunyaroni@gmail.com>
2025-01-08 19:27:03 +0530

    flask integration

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 from flask import *
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'stumble'
+
+mysql = MySQL(app)
+
+@app.route('/home')
+def index():
+    return render_template('home.html')
+
+app.run(debug=True, port=5000)
